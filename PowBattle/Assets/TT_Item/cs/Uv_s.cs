d74b55b2685d6fc6e385682de0a2dd4d6d1b7523
hashimor625 <hashimor625@gmail.com>
2017-08-18 00:51:33 +0900

    UvScroll改造

--- PowBattle/Assets/TT_Item/cs/Uv_s.cs
+++ PowBattle/Assets/TT_Item/cs/Uv_s.cs
@@ -4,15 +4,23 @@ using System.Collections;
 public class Uv_s : MonoBehaviour
 {
     [SerializeField]
+    private float scrollTime;
+    [SerializeField]
     private float scrollSpeedX = 0.1f;
     [SerializeField]
     private float scrollSpeedY = 0.1f;
 
     private Renderer ren;
     private Vector2 defaultOffset = Vector2.zero;
+    private float leftScrollTime;
 
     void Update()
     {
+        if (scrollTime > 0)
+        {
+            if (leftScrollTime <= 0) return;
+            leftScrollTime -= Time.deltaTime;
+        }
         float x = Mathf.Repeat(Time.time * scrollSpeedX, 1);
         float y = Mathf.Repeat(Time.time * scrollSpeedY, 1);
         SetOffset(new Vector2(x, y));
@@ -27,6 +35,7 @@ public class Uv_s : MonoBehaviour
     private void OnEnable()
     {
         ren = GetComponent<Renderer>();
+        leftScrollTime = scrollTime;
         //defaultOffset = ren.sharedMaterial.GetTextureOffset("_MainTex");
     }
 
